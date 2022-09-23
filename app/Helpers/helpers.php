93aaa702210fbeb5c0ef90f3bdb6cd2cab0e9c53
Elbek <you@example.com>
2022-09-23 17:16:38 +0500

    upd

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,12 +4,12 @@
 function getContentText($page, $key)
 {
     $text = app()->getLocale() == 'ru' ? 'text_ru' : 'text_uz';
-    return \App\Models\Content::query()->where('page', $page)->where('key', $key)->first()->$text;
+    return \App\Models\Content::query()->where('page', $page)->where('key', $key)->first()->$text ?? "";
 }
 
 function getContentImage($page, $key): string
 {
-    $path = \App\Models\Content::query()->where('page', $page)->where('key', $key)->first()->image;
+    $path = \App\Models\Content::query()->where('page', $page)->where('key', $key)->first()->image ?? "";
     $path = str_replace('\\', '/', $path);
     return asset('storage/' . $path);
 }
